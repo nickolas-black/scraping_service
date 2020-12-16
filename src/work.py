e8995a2c72f8a856edc3e4cd846aa612d7209792
BLACK <blacknickolas14@gmail.com>
2020-12-16 21:19:42 +0300

    сборщик с jobs.dou.ua

--- src/work.py
+++ src/work.py
@@ -74,9 +74,40 @@ def rabota(url):
 
     return jobs, errors
 
+
+def dou(url):
+    jobs = []
+    errors = []
+    resp = requests.get(url, headers=headers)
+    if resp.status_code == 200:
+        soup = BS(resp.content, 'html.parser')
+        main_div = soup.find('div', id='vacancyListId')
+        if main_div:
+            li_lst = main_div.find_all('li', attrs={'class': 'l-vacancy'})
+            for li in li_lst:
+                if '__hot' not in li['class']:
+                    title = li.find('div', attrs={'class': 'title'})
+                    href = title.a['href']
+                    cont = li.find('div', attrs={'class': 'sh-info'})
+                    content = cont.text
+                    company = 'No name'
+                    a = title.find('a', attrs={'class': 'company'})
+                    if a:
+                        company = a.text
+                    jobs.append({'title': title.text, 'url': href,
+                                 'description': content, 'company': company})
+
+        else:
+            errors.append({'url': url, 'title': "Div does not exists"})
+    else:
+        errors.append({'url': url, 'title': "Page do not response"})
+
+    return  jobs, errors
+
+
 if __name__ == '__main__':
-    url = 'https://rabota.ua/zapros/python/%d0%ba%d0%b8%d0%b5%d0%b2'
-    jobs, errors = rabota(url)
+    url = 'https://jobs.dou.ua/vacancies/?city=%D0%9A%D0%B8%D0%B5%D0%B2&category=Python'
+    jobs, errors = dou(url)
     h = codecs.open('work.json', 'w', 'utf-8')
     h.write(str(jobs))
     h.close()
