6deda27fe9f959f7ab5860314d579afde1fcc797
BLACK <blacknickolas14@gmail.com>
2020-12-19 13:38:03 +0300

    прикрутил функцию для сбора

--- src/scraping/models.py
+++ src/scraping/models.py
@@ -1,12 +1,18 @@
+import jsonfield
 from django.db import models
 
 from scraping.utils import from_cyrillic_to_eng
 
 
+def default_urls():
+    return {"work": "", "rabota": "", "dou": "", "djinni": ""}
+
+
 class City(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Название населенного пункта',
+    name = models.CharField(max_length=50,
+                            verbose_name='Название населенного пункта',
                             unique=True)
-    slug = models.CharField(max_length=100, blank=True, unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
 
     class Meta:
         verbose_name = 'Название населенного пункта'
@@ -22,9 +28,10 @@ class City(models.Model):
 
 
 class Language(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Язык программирования',
+    name = models.CharField(max_length=50,
+                            verbose_name='Язык программирования',
                             unique=True)
-    slug = models.CharField(max_length=100, blank=True, unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
 
     class Meta:
         verbose_name = 'Язык программирования'
@@ -44,13 +51,35 @@ class Vacancy(models.Model):
     title = models.CharField(max_length=250, verbose_name='Заголовок вакансии')
     company = models.CharField(max_length=250, verbose_name='Компания')
     description = models.TextField(verbose_name='Описание вакансии')
-    city = models.ForeignKey('City', on_delete=models.CASCADE, verbose_name='Город')
-    language = models.ForeignKey('Language',  on_delete=models.CASCADE, verbose_name='Язык программирования')
+    city = models.ForeignKey('City', on_delete=models.CASCADE,
+                             verbose_name='Город', related_name='vacancies')
+    language = models.ForeignKey('Language', on_delete=models.CASCADE,
+                                 verbose_name='Язык программирования')
     timestamp = models.DateField(auto_now_add=True)
 
     class Meta:
         verbose_name = 'Вакансия'
         verbose_name_plural = 'Вакансии'
+        ordering = ['-timestamp']
 
     def __str__(self):
         return self.title
+
+
+class Error(models.Model):
+    timestamp = models.DateField(auto_now_add=True)
+    data = jsonfield.JSONField()
+
+    def __str__(self):
+        return str(self.timestamp)
+
+
+class Url(models.Model):
+    city = models.ForeignKey('City', on_delete=models.CASCADE,
+                             verbose_name='Город')
+    language = models.ForeignKey('Language', on_delete=models.CASCADE,
+                                 verbose_name='Язык программирования')
+    url_data = jsonfield.JSONField(default=default_urls)
+
+    class Meta:
+        unique_together = ("city", "language")
