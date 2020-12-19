3ab253b4d9d848b5f64b96e7821a25ecebe2070c
BLACK <blacknickolas14@gmail.com>
2020-12-19 16:10:11 +0300

    подправил получение данных и добавление в БД

--- src/scraping/admin.py
+++ src/scraping/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import City, Vacancy, Language, Error
+from .models import City, Vacancy, Language, Error, Url
 
 
 admin.site.register(City)
 admin.site.register(Language)
 admin.site.register(Vacancy)
 admin.site.register(Error)
+admin.site.register(Url)
