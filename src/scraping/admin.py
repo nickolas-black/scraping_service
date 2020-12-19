8fde7f12e1b6c83379e86f47ddff07fadbcdc53e
BLACK <blacknickolas14@gmail.com>
2020-12-19 14:25:19 +0300

    добавил json

--- src/scraping/admin.py
+++ src/scraping/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import City, Vacancy, Language
+from .models import City, Vacancy, Language, Error
 
 
 admin.site.register(City)
 admin.site.register(Language)
 admin.site.register(Vacancy)
+admin.site.register(Error)
